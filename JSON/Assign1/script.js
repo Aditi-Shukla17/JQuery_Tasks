f926b08962e402ea36d3f5fc6c35eb8d53bf9ee6
Aditi-Shukla17 <aditishukla01.hp@outlook.com>
2024-08-05 11:00:10 +0530

    did some changes

--- JSON/Assign1/script.js
+++ JSON/Assign1/script.js
@@ -1,40 +1,23 @@
-function fetchPersonData() {
-  $.ajax({
-    url: 'data.json',
-    method: 'GET',
-    dataType: 'json',
-    success: function (obj) {
-      $('#fname').text(obj.fname);
-      //console.log(obj.fname)
-      $('#lname').text(obj.lname);
-      $('#dob').text(obj.dob);
-    },
-    error: function ( textStatus) {
-      console.error("Error fetching the JSON data:", textStatus);
+async function fetchPersonData() {
+  try {
+    const response = await fetch("data.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-  });
+    const obj = await response.json();
+    //console.log(obj);
+
+    document.getElementById("fname").textContent = obj.fname;
+    document.getElementById("lname").textContent = obj.lname;
+    document.getElementById("dob").textContent = obj.dob;
+  } catch (error) {
+    console.error("Error fetching the JSON data:", error);
+  }
 }
 
-$(document).ready(function () {
-  fetchPersonData();
-});
+fetchPersonData();
 
 
-// async function fetchPersonData() {
-//   try {
-//     const response = await fetch("data.json");
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     const obj = await response.json();
-//     //console.log(obj);
 
-//     document.getElementById("fname").textContent = obj.fname;
-//     document.getElementById("lname").textContent = obj.lname;
-//     document.getElementById("dob").textContent = obj.dob;
-//   } catch (error) {
-//     console.error("Error fetching the JSON data:", error);
-//   }
-// }
 
-// fetchPersonData();
+
